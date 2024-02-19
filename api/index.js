6dc6483ddb6d80f51e51401bc5288e6da4651853
kaif shaikh <kaaifshaikh22@gmail.com>
2024-02-19 20:00:36 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import  authRoutes  from './routes/auth.route.js';
 
 
 dotenv.config();
@@ -17,6 +18,8 @@ mongoose.connect(process.env.MONGO)
 
 const app = express();
 
+app.use(express.json());
+
 
 
 app.listen(3000, () => {
@@ -24,3 +27,5 @@ app.listen(3000, () => {
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
