60d75b2d592b5bb7e182f4b932540b479c858110
kaif shaikh <kaaifshaikh22@gmail.com>
2024-02-25 16:01:09 +0530

    complete dark mode functionality

---
+++ client/src/components/ThemeProvider.jsx
@@ -0,0 +1,15 @@
+import {useSelector} from 'react-redux'
+
+export default function ThemeProvider({children}) {
+    const {theme} = useSelector(state => state.theme)
+  return (
+    <div className={theme}>
+        <div className='dark:text-gray-200 dark:bg-[rgb(16,23,42)] bg-white text-gray-700
+        min-h-screen'>
+        {children}
+        </div>
+        
+
+    </div>
+  )
+}
