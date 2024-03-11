af080ab17680b50ec12e9bcb2e6c5b967dea68cb
kaif shaikh <kaaifshaikh22@gmail.com>
2024-03-11 15:34:13 +0530

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center
+          my-7">About Kaif' Blog</h1>
+          <div className="text-md text-gray-600 flex flex-col gap-6">
+            <p>
+            Hey fellow code enthusiasts! 👋 Excited to have you step into my digital realm, 
+            where lines of code transform ideas into reality.
+            I hope you enjoy reading my blog.
+            </p>
+            <p>
+            Join me on a journey through the ever-evolving landscape of programming, as we unravel the mysteries of algorithms, 
+            debug the challenges, and celebrate the joy of creating. 
+            From novice coders to seasoned developers, 
+            Kaif Blog Coding is your go-to source for insights, tutorials, and a dash of coding camaraderie.
+            </p>
+            <p>
+            Ready to dive into the world where logic meets creativity? 
+            Let's code something extraordinary together!
+            </p>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
   )
 }
